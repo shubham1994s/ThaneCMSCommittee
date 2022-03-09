072763854026bfbe746426e9a055f462675d44e9
roshan1990s <r16smile@gmail.com>
2022-03-09 16:39:02 +0530

    comit by roshan

--- SwachBharat.CMS.Bll.Repository/GridRepository/DashBoardRepository.cs
+++ SwachBharat.CMS.Bll.Repository/GridRepository/DashBoardRepository.cs
@@ -2841,6 +2841,8 @@ namespace SwachBharat.CMS.Bll.Repository.GridRepository
                     batteryStatus = x.batteryStatus,
                     los = x.los,
                     ctype = x.CType,
+                    wet=x.Wet,
+                    dry=x.Dry,
 
 
                 }).OrderByDescending(c => c.gcDate).ToList().ToList();
