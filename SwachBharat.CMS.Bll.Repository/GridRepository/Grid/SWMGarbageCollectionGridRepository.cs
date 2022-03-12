9b105ab68fe12f0fa71582783b43558d5378520f
roshan1990s <r16smile@gmail.com>
2022-03-12 11:51:37 +0530

    comit by roshan

--- SwachBharat.CMS.Bll.Repository/GridRepository/Grid/SWMGarbageCollectionGridRepository.cs
+++ SwachBharat.CMS.Bll.Repository/GridRepository/Grid/SWMGarbageCollectionGridRepository.cs
@@ -13,9 +13,9 @@ namespace SwachBharat.CMS.Bll.Repository.GridRepository.Grid
 
         DashBoardRepository objRep = new DashBoardRepository();
 
-        public SWMGarbageCollectionGridRepository(long wildcard, string SearchString, DateTime? fdate, DateTime? tdate, int userId, int appId, int? param1, int? param2, int? param3, int? param4)
+        public SWMGarbageCollectionGridRepository(long wildcard, string SearchString, DateTime? fdate, DateTime? tdate, int userId, int appId, int? param1, int? param2, int? param3, int? param4, int? param6)
         {
-            dataSet = objRep.GetSWMGarbageCollectionData(wildcard, SearchString, fdate, tdate, userId, appId, param1, param2, param3, param4);
+            dataSet = objRep.GetSWMGarbageCollectionData(wildcard, SearchString, fdate, tdate, userId, appId, param1, param2, param3, param4, param6);
         }
 
         public string GetDataTabelJson(string sortColumn, string sortColumnDir, string draw, string length, string searchValue, string start)
