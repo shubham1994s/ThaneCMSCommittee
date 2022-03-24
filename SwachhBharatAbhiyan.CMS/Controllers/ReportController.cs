5a3ea10f148fdb9a995303543f8e979ffcd73476
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-03-24 12:43:29 +0530

    Comit By Rohit

--- SwachhBharatAbhiyan.CMS/Controllers/ReportController.cs
+++ SwachhBharatAbhiyan.CMS/Controllers/ReportController.cs
@@ -273,6 +273,34 @@ namespace SwachhBharatAbhiyan.CMS.Controllers
             else
                 return Redirect("/Account/Login");
         }
+
+
+        public ActionResult MenuSWMReportIndex()
+        {
+            if (SessionHandler.Current.AppId != 0)
+            {
+                return View();
+            }
+            else
+                return Redirect("/Account/Login");
+        }
+
+        public ActionResult SWMReportIndex()
+        {
+
+            if (SessionHandler.Current.AppId != 0)
+            {
+                Session["NewAppID"] = SessionHandler.Current.AppId;
+                Session["DB_Name"] = SessionHandler.Current.DB_Name;
+                string Reportname = "ss";
+
+                ViewBag.IframeUrl = "/DisplayReports.aspx?FromDate=" + DateTime.Now.ToString("MM/dd/yyyy");
+
+                return View();
+            }
+            else
+                return Redirect("/Account/Login");
+        }
         // Pointwise Reports
 
         #region 1.1
