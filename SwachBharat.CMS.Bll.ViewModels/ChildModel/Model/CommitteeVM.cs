7f78da655a70cae69919b3809454b500fc4e7fba
roshan1990s <r16smile@gmail.com>
2022-07-30 14:55:59 +0530

    Comit by roshan

---
+++ SwachBharat.CMS.Bll.ViewModels/ChildModel/Model/CommitteeVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SwachBharat.CMS.Bll.ViewModels.ChildModel.Model
+{
+    public class CommitteeVM : BaseVM
+    {
+        public int Id { get; set; }
+        [Remote("CheckCommitteeDetails", "MainMaster", HttpMethod = "POST", ErrorMessage = "Name already exists!", AdditionalFields = "Id")]
+        public string CommitteeNo { get; set; }
+
+        public Nullable<int> zoneId { get; set; }
+    }
+}
