684adc3b88cb72f17bd2c2540de817692ea43497
roshan1990s <r16smile@gmail.com>
2022-08-01 12:11:54 +0530

    Comit by roshan

--- SwachBharat.CMS.Bll.ViewModels/ChildModel/Model/WardNumberVM.cs
+++ SwachBharat.CMS.Bll.ViewModels/ChildModel/Model/WardNumberVM.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using System.Workflow.ComponentModel.Compiler;
 
 namespace SwachBharat.CMS.Bll.ViewModels.ChildModel.Model
 {
     public class WardNumberVM : BaseVM
     {
         public int Id { get; set; }
-        [Remote("CheckWardDetails", "MainMaster", HttpMethod = "POST", ErrorMessage = "Name already exists!", AdditionalFields = "Id")]
+        //[Remote("CheckWardDetails", "MainMaster", HttpMethod = "POST", ErrorMessage = "Name already exists!", AdditionalFields = "Id")]
         public string WardNo { get; set; }
 
         public Nullable<int> zoneId { get; set; }
