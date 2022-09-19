6d33d9d1b003f5dee2b9739d0b9da3ba9d01e10e
shubham1994s <shubhamkawle41@gmail.com>
2022-09-19 14:52:52 -0700

    comit by shubham

--- SwachhBharatAbhiyan.CMS/Content/DatatableGrid/EmployeeDetailsGrid.js
+++ SwachhBharatAbhiyan.CMS/Content/DatatableGrid/EmployeeDetailsGrid.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('input[type=radio][name=rdType]').change(function () {
         LoadGrid();
     });
-    LoadGrid();
+   LoadGrid();
     // $('#demoGrid').css("display", "block");
     $('#btn').hide();
 
@@ -250,68 +250,7 @@ function EmployeeCT() {
 }
 
 
-//function ActiveEmployee() {
-//    $("#demoGridActive").DataTable({
-//        "sDom": "ltipr",
-//        "order": [[0, "desc"]],
-//        "processing": true, // for show progress bar
-//        "serverSide": true, // for process server side
-//        "filter": true, // this is for disable filter (search box)
-//        "orderMulti": false, // for disable multiple column at once
-//        "pageLength": 10,
-//        destroy: true,
 
-//        "ajax": {
-//            "url": "/Datable/GetJqGridJson?rn=ActiveEmployee",
-//            "type": "POST",
-//            "datatype": "json"
-//        },
-
-//        "columnDefs":
-//            [{
-//                "targets": [0],
-//                "visible": false,
-//                "searchable": false
-//            },
-//            {
-//                "targets": [2],
-//                "visible": false,
-//                "searchable": false
-//            },
-//            {
-//                "targets": [5],
-//                "visible": true,
-
-//                "render": function (data, type, full, meta) {
-//                    if (full["userProfileImage"] != "/Images/default_not_upload.png") {
-//                        return "<div style='cursor:pointer;display:inline-flex;'  onclick=PopImages(this)><img alt='Photo Not Found'  src='" + data +
-//                            "' style='height:35px;width:35px;cursor:pointer;margin-left:0px;'></img><span><ul class='dt_pop'  style='margin:2px -5px -5px -5px; padding:0px;list-style:none;display:none;'><li  class='li_date datediv' >" + full["attandDate"] + "</li><li class='addr-length' style='margin:0px 0px 0px 10px;'>"
-//                            + full["Address"] + "</li><li style='display:none' class='li_title' >Image </li></ul></span></div>";
-//                    }
-//                    else {
-
-//                        return "<img alt='Photo Not Found' onclick='noImageNotification()' src='/Images/default_not_upload.png' style='height:35px;width:35px;cursor:pointer;'></img>";
-//                    }
-//                },
-//            }
-//            ],
-
-//        "columns": [
-//            { "data": "userId", "name": "userId", "autoWidth": false },
-//            { "data": "userName", "name": "userName", "width": "30%" },
-//            { "data": "userNameMar", "name": "userNameMar", "width": "20%" },
-//            { "data": "userMobileNumber", "name": "userMobileNumber", "width": "13%" },
-//            { "data": "userEmployeeNo", "name": "userEmployeeNo", "width": "15%" },
-//            { "data": "userProfileImage", "name": "userProfileImage", "width": "15%" },
-//            { "data": "userAddress", "name": "userAddress", "width": "25%" },
-//            { "data": "bloodGroup", "name": "bloodGroup", "width": "25%" },
-//            { "data": "isActive", "name": "isActive", "width": "25%" },
-//            { "data": "gcTarget", "name": "gcTarget", "width": "25%" },
-//            { "render": function (data, type, full, meta) { return '<a  data-toggle="modal" class="tooltip1" style="cursor:pointer"   onclick="Edit(' + full["userId"] + ')"  ><i class="material-icons edit-icon">edit</i><span class="tooltiptext1">Edit</span> </a>'; }, "width": "10%" },
-//            //<a  data-toggle="modal" style="cursor:pointer;margin-left:10px;" class="tooltip1" style="cursor:pointer"onclick="Delete(' + full["userId"] + ')" ><i class="material-icons delete-icon">delete</i><span class="tooltiptext1">Delete</span> </a>
-//        ]
-//    })
-//}
 
 function NotActiveEmployeeW() {
     debugger;
